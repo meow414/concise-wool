c92a4f65451977172abb3365eda3fd28fb5e4742
Glitch (concise-wool) <none>
2019-07-06 14:31:12 +0000

    🚴🍉 Checkpoint ./server.js:5325173/146

--- server.js
+++ server.js
@@ -76,7 +76,8 @@ app.get('/api/exercise/log',(req,res,next)=>{
                                                // console.log(logArray)
          let logArray= data[0].log.filter((a)=>{
                                                if(from&&to)
-                                                 return (new Date(a.date)>=new Date(from))
+                                                 return (new Date(a.date)>=new Date(from))&&(new Date(a.date)<=new Date(to))
+                                                if(from)return (new Date(a.date)>=new Date(from))
                                                })
                                                console.log(logArray)
               if(err) throw (err);
