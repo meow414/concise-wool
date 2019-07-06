ae0e77dd68e7a1db67ee14acae22f80cb760a501
Glitch (concise-wool) <none>
2019-07-06 16:09:03 +0000

    👨‍❤️‍👨👓 Checkpoint ./server.js:5325173/735

--- server.js
+++ server.js
@@ -69,7 +69,13 @@ app.get('/api/exercise/log',(req,res,next)=>{
    res.send("Please pass userId");
   }
   else {//else starting
-    consol.log("hi")
+       userData.findById({ _id: userId }, (err, data) => {console.log(data)
+     if(err) throw err;
+     if(data){ return res.send("username already exists")}
+     else{
+      return res.send("userId does not exist")
+     }
+   })
        // userData.find({_id:userId},function(err,data){
        //   if(err) throw err;
        //   if(data!==undefined){
