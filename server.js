09ba4769b125e5f3f296af44c40387b78d135381
Glitch (concise-wool) <none>
2019-07-06 06:13:02 +0000

    😜🐇 Checkpoint ./server.js:5325173/311

--- server.js
+++ server.js
@@ -36,12 +36,17 @@ app.post('/api/exercise/new-user',(req,res,next)=>{
 
 app.post('/api/exercise/add',(req,res,next)=>{
   console.log(req.body) //count also
-  userData.findOneAndUpdate({_id:req.body.userId},{$inc:{count:1},$set:{log:[{description:req.body.description,duration:req.body.duration,date:req.body.date||Date().toDateString()}]}},(err,data)=>{
-    if(err) throw err;
-   if(data){
-   res.json({username:data.username,userId:data._id,count:data.count+1,log:[{"description":req.body.description,"duration":req.body.duration,"date":req.body.date||Date().toDateString()}]})
-   }
-   });
+  let d;
+  if(req.body.date==null){
+    d = new Date();
+    d = d.toDateString();
+  } console.log(d)
+  // userData.findOneAndUpdate({_id:req.body.userId},{$inc:{count:1},$set:{log:[{description:req.body.description,duration:req.body.duration,date:req.body.date||d}]}},(err,data)=>{
+  //   if(err) throw err;
+  //  if(data){
+  //  res.json({username:data.username,userId:data._id,count:data.count+1,log:[{"description":req.body.description,"duration":req.body.duration,"date":req.body.date||d}]})
+  //  }
+  //  });
 })
 
 // Not found middleware
